c4ca4e7c377b788110bf2e3d05e15aa3b3bf63a1
valsou <valslite@gmail.com>
2018-04-21 02:41:13 +0200

    fix LAT/LONG const

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@
 // # Please replace with your own informations
 // #######################################
 // # URL of your slide (Google Slide or another with 16/10 iframe)
-const SLIDE_URL = "HTTP://URL-OF-YOUR-SLIDE";
+const SLIDE_URL = "https://docs.google.com/presentation/d/e/2PACX-1vQoELt5p3el0Z4v0TOCFjnJCucY8PEZGpSgtZprx8zM1eFt-SpVmP3XuBGIufALVohhtb2MXg7KV3l2/embed?start=true&loop=true&delayms=3000&rm=minimal";
 // # Latitude of the city you want weather
-const LATITUDE = 0;
+const LATITUDE = 45.17921;
 // # Longitude of the city you want weather
-const LONGITUDE = 0;
+const LONGITUDE = 5.73145;
 // # Elevation above the sea of the city you want weather
-const ELEVATION = 0;
+const ELEVATION = 215;
